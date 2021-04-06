8704a5fad27cff6d45d39a192e193e9cbdd240e6
katarzyna_w <katarzyna.koch@gmail.com>
2021-04-05 19:27:18 -0500

    remove morning message

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -1,13 +1,9 @@
 import '../App.css';
 
 function Survey() {
-  const goodMorning = 'Good morning, sunshine';
   return (
     <div class="container-survey">
     <div className="wrapper-survey">
-        <p>
-        {goodMorning}
-        </p>
         <form className="form">
           <label className="label">
             Date: 
