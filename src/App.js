b75a0e4b585fb9ab3ec3748d641182d12237f456
katarzyna_w <katarzyna.koch@gmail.com>
2021-04-05 20:00:07 -0500

    useState in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React, { useState } from 'react';
 import Header from './components/Header.js';
 import Survey from './components/Survey.js';
 import Footer from './components/Footer.js';
 import './App.css';
 
 function App() {
+  const [date, setDate] = useState('');
+  const [mood, setMood] = useState('');
+
+
   return (
     <div className="App">
       <Header />
--- src/components/Survey.js
+++ src/components/Survey.js
@@ -3,23 +3,23 @@ import '../App.css';
 function Survey() {
   return (
     <div class="container-survey">
-    <div className="wrapper-survey">
-        <form className="form">
-          <label className="label">
-            Date
-          </label>
-          <input className="input" type="text" />
-          <br></br>
-          <label className="label">
-            Mood
-          </label>
-          <input className="input" type="text" />
-          <br></br>
-          <div className="buttonWrapper">
-            <button>submit</button>
-          </div>
-        </form>
-    </div>
+      <div className="wrapper-survey">
+          <form className="form">
+            <label className="label">
+              Date
+            </label>
+            <input className="input" type="date" />
+            <br></br>
+            <label className="label">
+              Mood
+            </label>
+            <input className="input" type="text" />
+            <br></br>
+            <div className="buttonWrapper">
+              <button>submit</button>
+            </div>
+          </form>
+      </div>
     </div>
   );
 }
