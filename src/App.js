acdd72dba9f7689082a007f6ec9b30bba8d5b6b1
katarzyna_w <katarzyna.koch@gmail.com>
2021-04-06 20:03:47 -0500

    handleClick hook in App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import Header from './components/Header.js';
 import Survey from './components/Survey.js';
 import Footer from './components/Footer.js';
 import './App.css';
+import axios from 'axios'
 
 function App() {
   const [date, setDate] = useState('');
   const [mood, setMood] = useState('');
 
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    const obj = { date, mood };
+
+    axios
+      .post(
+        'https://sheet.best/api/sheets/5eb84415-bab5-467b-a005-8a4caa4766f3', 
+        obj
+      )
+      .then((response) => {
+        console.log(response);
+      });
+  };
 
   return (
     <div className="App">
