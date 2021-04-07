88dcdb40fef61665e0aaad947bb6af3fafd43479
katarzyna_w <katarzyna.koch@gmail.com>
2021-04-06 20:31:06 -0500

    move handleClick and setState hooks to Survey.js from App.js

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -1,14 +1,40 @@
+import React, { useState } from 'react';
+import axios from 'axios'
 import '../App.css';
 
 function Survey() {
+  const [date, setDate] = useState('');
+  const [mood, setMood] = useState('');
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    const obj = { date, mood };
+
+    axios
+      .post(
+        'https://sheet.best/api/sheets/5eb84415-bab5-467b-a005-8a4caa4766f3', 
+        obj
+      )
+      .then((response) => {
+        console.log(response);
+      });
+  };
+
   return (
     <div class="container-survey">
       <div className="wrapper-survey">
           <form className="form">
-            <label className="label">
+            <label 
+              className="label"
+              >
               Date
             </label>
-            <input className="input" type="date" />
+            <input 
+              className="input" 
+              type="date" 
+              onChange={(e) => setDate(e.target.value)}
+            />
             <br></br>
             <label className="label">
               Mood
