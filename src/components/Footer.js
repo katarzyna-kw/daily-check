ef5c83b2de35dfd4b43ebf4c09919fe252d33a0b
katarzyna-kw <katarzyna.koch@gmail.com>
2022-05-07 13:31:52 -0500

    added link/styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import '../App.css';
 function Footer() {
   return (
     <div className="wrapper-footer">
-        2021, Katarzyna-KW.
+        2021, <a href="https://katarzyna-kw.github.io/portfolio-website/" target="blank" rel="noreferrer">Katarzyna Wegrzynowicz</a>
     </div>
   );
 }
